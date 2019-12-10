669bae673bbac537a98bb45b5192f0dd0abadd3a
Phill Serrazina <fserrazina99@gmail.com>
2019-12-10 20:04:58 +0000

    Basic Project Setup

---
+++ DayAtChilltimeProject/Assets/Scripts/Card.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public enum CardStates { Checked, Unchecked }
+
+    [SerializeField] private Sprite cardSprite;
+    [SerializeField] private CardStates currentState;
+}
---
+++ DayAtChilltimeProject/Assets/Scripts/ImageGroupInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Helper class that renders a given image a given number of times
+/// at the start of the scene instead of making the developer do it by hand.
+/// </summary>
+public class ImageGroupInitializer : MonoBehaviour
+{
+    [SerializeField] private GameObject imageObject = null;
+    [SerializeField] private int numberOfObjects = 0;
+
+    private void Awake() {
+        for (int i = 0; i < numberOfObjects; i++) {
+            Instantiate(imageObject, transform);
+        }
+    }
+}
